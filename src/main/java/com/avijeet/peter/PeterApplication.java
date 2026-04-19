0e4840ce84fc3ed1ce064591e9fb7b0da50846c4
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-04-19 21:57:54 +0530

    add:GraphQL dependencies and setup

---
+++ src/main/java/com/avijeet/peter/PeterApplication.java
@@ -0,0 +1,13 @@
+package com.avijeet.peter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeterApplication.class, args);
+    }
+
+}
